989b40096399837a1e0a63a4fbd212d620a3a20f
Megan Ellman <meganlellman@gmail.com>
2023-01-25 14:13:48 -0800

    passed first four tests

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -1,5 +1,20 @@
 class Letter {
-
+    constructor(letter) {
+        this.letter = letter;
+        const regex = new RegExp('[a-z0-9A-Z]')
+        if (regex.test(this.letter)) {
+            this.visible = false;
+        } else {
+            this.visible = true;
+        };
+    }
+    toString(){
+        if (this.visible === true) {
+            return this.letter
+        } else {
+            return '_'
+        }
+    }
 }
 
 module.exports = Letter;
